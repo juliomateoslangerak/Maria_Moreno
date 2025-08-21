5d77eee8a78adda3a6239e20b765dfb35428eec9
Julio Mateos Langerak <julio.mateos-langerak@igh.cnrs.fr>
2025-08-21 07:22:18 +0200

    Doing all channels Average projecting time

--- orthogonal_projector.py
+++ orthogonal_projector.py
@@ -17,6 +17,7 @@ HOST = 'omero.mri.cnrs.fr'
 PORT = 4064
 GROUP = "Jouvence 3D-SIM"
 dataset_id = 25502
+voxel_size = None
 
 
 def airy_fun(
@@ -159,19 +160,19 @@ def extract_profiles(
     profiles = {
         "z": plot_profile(
             xz, profile_z, fit_z, "Z profile", "r", "g", {
-                "FWHM": f"{fwhm_z * pixel_size[0]:.2f}",
+                "FWHM": f"{fwhm_z * voxel_size[0]:.2f}",
                 "R^2": f"{cd_r2_z:.2f}",
             }
         ),
         "y": plot_profile(
             xy, profile_y, fit_y, "Y profile", "r", "g", {
-                "FWHM": f"{fwhm_y * pixel_size[1]:.2f}",
+                "FWHM": f"{fwhm_y * voxel_size[1]:.2f}",
                 "R^2": f"{cd_r2_y:.3f}",
             }
         ),
         "x": plot_profile(
             xx, profile_x, fit_x, "X profile", "r", "g", {
-                "FWHM": f"{fwhm_x * pixel_size[2]:.2f}",
+                "FWHM": f"{fwhm_x * voxel_size[2]:.2f}",
                 "R^2": f"{cd_r2_x:.3f}",
             }
         )
@@ -199,7 +200,8 @@ try:
 
     for image in images:
         image_intensities = None
-        pixel_size = ot.get_pixel_size(image, order='zyx')
+        if voxel_size is None:
+            voxel_size = ot.get_pixel_size(image, order='zyx')
 
         rois = roi_service.findByImage(image.getId(), None).rois
         for roi in rois:
@@ -209,160 +211,169 @@ try:
                     z_pos = shape.getTheZ()._val
                 except AttributeError:
                     z_pos = 0
-                try:
-                    t_pos = shape.getTheT()._val
-                except AttributeError:
-                    t_pos = 0
+                t_pos = 0  # Assuming time point is always 0, can be modified if needed
                 try:
                     c_pos = shape.getTheC()._val
                 except AttributeError:
-                    c_pos = 0
+                    c_pos = None
                 y_pos = shape.getY()._val
                 x_pos = shape.getX()._val
                 try:
                     shape_comment = shape.getTextValue()._val
                 except AttributeError:
-                    shape_comment = f"Point_{z_pos}_{c_pos}_{t_pos}_{y_pos:.2f}_{x_pos:.2f}"
+                    shape_comment = f"Point_at_{z_pos}_{y_pos:.2f}_{x_pos:.2f}"
 
                 if image_intensities is None:
                     image_intensities = ot.get_intensities(image)
-
-                # dims in order z, c, t, y, x
-                yx_slice = image_intensities[z_pos, c_pos, t_pos, :, :]
-                yx_slice = np.reshape(yx_slice, (1, 1, 1, yx_slice.shape[0], yx_slice.shape[1]))
-                yx_image = ot.create_image_from_numpy_array(
-                    connection=conn,
-                    data=yx_slice,
-                    image_name=f"{image.getName()}_{shape_comment}_yx",
-                    image_description=f"yx Orthogonal slice at Z={z_pos}, C={c_pos}, T={t_pos}. source imageId:{image.getId()}",
-                    # channel_labels=None,
-                    dataset=dataset,
-                    source_image_id=image.getId(),
-                    # channels_list=None,
-                    force_whole_planes=True
-                )
-                yx_image.unload()
-                yx_line_roi = ot.create_roi(
-                    connection=conn,
-                    image=yx_image,
-                    shapes=[
-                        ot.create_shape_line(
-                            x1_pos=0,
-                            y1_pos=y_pos,
-                            x2_pos=yx_slice.shape[4],
-                            y2_pos=y_pos,
-                            stroke_color=(255, 0, 0, 200),
-                            stroke_width=0.5,
-                        ),
-                        ot.create_shape_line(
-                            x1_pos=x_pos,
-                            y1_pos=0,
-                            x2_pos=x_pos,
-                            y2_pos=yx_slice.shape[3],
-                            stroke_color=(255, 0, 0, 200),
-                            stroke_width=0.5,
-                        )
-                    ]
-                )
-
-                zx_slice = image_intensities[:, c_pos, t_pos, int(y_pos), :]
-                zx_slice = rescale(zx_slice, (pixel_size[0] / pixel_size[2], 1), anti_aliasing=True, preserve_range=True)
-                zx_slice = zx_slice.astype(image_intensities.dtype)
-                zx_slice = np.reshape(zx_slice, (1, 1, 1, zx_slice.shape[0], zx_slice.shape[1]))
-                zx_image = ot.create_image_from_numpy_array(
-                    connection=conn,
-                    data=zx_slice,
-                    image_name=f"{image.getName()}_{shape_comment}_zx",
-                    image_description=f"zx Orthogonal slice at Z={z_pos}, C={c_pos}, T={t_pos}. source imageId:{image.getId()}",
-                    # channel_labels=None,
-                    dataset=dataset,
-                    source_image_id=image.getId(),
-                    # channels_list=None,
-                    force_whole_planes=True
-                )
-                zx_image.unload()
-                zx_line_roi = ot.create_roi(
-                    connection=conn,
-                    image=zx_image,
-                    shapes=[
-                        ot.create_shape_line(
-                            x1_pos=0,
-                            y1_pos=(z_pos - 0.5) * (pixel_size[0] / pixel_size[2]),
-                            x2_pos=zx_slice.shape[4],
-                            y2_pos=(z_pos - 0.5) * (pixel_size[0] / pixel_size[2]),
-                            stroke_color=(255, 0, 0, 200),
-                            stroke_width=0.5,
-                        ),
-                        ot.create_shape_line(
-                            x1_pos=x_pos,
-                            y1_pos=0,
-                            x2_pos=x_pos,
-                            y2_pos=zx_slice.shape[3],
-                            stroke_color=(255, 0, 0, 200),
-                            stroke_width=0.5,
-                        )
-                    ]
-                )
-
-                zy_slice = image_intensities[:, c_pos, t_pos, :, int(x_pos)]
-                zy_slice = rescale(zy_slice, (pixel_size[0] / pixel_size[1], 1), anti_aliasing=True, preserve_range=True)
-                zy_slice = zy_slice.astype(image_intensities.dtype)
-                zy_slice = np.transpose(zy_slice, (1, 0))  # Transpose to get the correct orientation
-                zy_slice = np.reshape(zy_slice, (1, 1, 1, zy_slice.shape[0], zy_slice.shape[1]))
-                zy_image = ot.create_image_from_numpy_array(
-                    connection=conn,
-                    data=zy_slice,
-                    image_name=f"{image.getName()}_{shape_comment}_zy",
-                    image_description=f"zy Orthogonal slice at Z={z_pos}, C={c_pos}, T={t_pos}. source imageId:{image.getId()}",
-                    # channel_labels=None,
-                    dataset=dataset,
-                    source_image_id=image.getId(),
-                    # channels_list=None,
-                    force_whole_planes=True
-                )
-                zy_image.unload()
-                zy_line_roi = ot.create_roi(
-                    connection=conn,
-                    image=zy_image,
-                    shapes=[
-                        ot.create_shape_line(
-                            x1_pos=0,
-                            y1_pos=y_pos,
-                            x2_pos=zy_slice.shape[4],
-                            y2_pos=y_pos,
-                            stroke_color=(255, 0, 0, 200),
-                            stroke_width=0.5,
-                        ),
-                        ot.create_shape_line(
-                            x1_pos=(z_pos - 0.5) * (pixel_size[0] / pixel_size[1]),
-                            y1_pos=0,
-                            x2_pos=(z_pos - 0.5) * (pixel_size[0] / pixel_size[1]),
-                            y2_pos=zy_slice.shape[3],
-                            stroke_color=(255, 0, 0, 200),
-                            stroke_width=0.5,
-                        )
-                    ]
-                )
-
-                profiles = extract_profiles(
-                    image_intensities[:, c_pos, t_pos, :, :],
-                    (z_pos, int(y_pos), int(x_pos)),
-                    21,
-                    voxel_size=pixel_size,)
-                for axis, profile_img in profiles.items():
-                    # Convert to RGB for OMERO
-                    omero_rgb_profile = np.transpose(profile_img, (2, 0, 1))
-                    omero_rgb_profile = omero_rgb_profile.reshape(
-                        (1, 3, 1, omero_rgb_profile.shape[1], omero_rgb_profile.shape[2])
-                    )
-                    profile_image = ot.create_image_from_numpy_array(
+                    # If we find a time lapse, this is probably the phases in teh raw image and we want to
+                    # average them
+                    if image_intensities.shape[2] > 1:
+                        image_intensities = np.mean(image_intensities, axis=2, keepdims=True)
+                    
+                if c_pos is None:
+                    c_positions = range(image_intensities.shape[1])
+                elif isinstance(c_pos, int):
+                    c_positions = [c_pos]
+                else:
+                    raise ValueError("c_pos should be an int or a list of ints")
+                    
+                for c_pos in c_positions:
+                    # dims in order z, c, t, y, x
+                    yx_slice = image_intensities[z_pos, c_pos, t_pos, :, :]
+                    yx_slice = np.reshape(yx_slice, (1, 1, 1, yx_slice.shape[0], yx_slice.shape[1]))
+                    yx_image = ot.create_image_from_numpy_array(
                         connection=conn,
-                        data=omero_rgb_profile,
-                        image_name=f"{image.getName()}_{shape_comment}_{axis}_profile",
-                        image_description=f"Profile along {axis} axis at Z={z_pos}, C={c_pos}, T={t_pos}. source imageId:{image.getId()}",
+                        data=yx_slice,
+                        image_name=f"{image.getName()}_{shape_comment}_C{c_pos}_yx",
+                        image_description=f"yx Orthogonal slice at Z={z_pos}, C={c_pos}, T={t_pos}. source imageId:{image.getId()}",
+                        # channel_labels=None,
                         dataset=dataset,
+                        source_image_id=image.getId(),
+                        # channels_list=None,
+                        force_whole_planes=True
                     )
-                    profile_image.unload()
+                    yx_image.unload()
+                    yx_line_roi = ot.create_roi(
+                        connection=conn,
+                        image=yx_image,
+                        shapes=[
+                            ot.create_shape_line(
+                                x1_pos=0,
+                                y1_pos=y_pos,
+                                x2_pos=yx_slice.shape[4],
+                                y2_pos=y_pos,
+                                stroke_color=(255, 0, 0, 200),
+                                stroke_width=0.5,
+                            ),
+                            ot.create_shape_line(
+                                x1_pos=x_pos,
+                                y1_pos=0,
+                                x2_pos=x_pos,
+                                y2_pos=yx_slice.shape[3],
+                                stroke_color=(255, 0, 0, 200),
+                                stroke_width=0.5,
+                            )
+                        ]
+                    )
+    
+                    zx_slice = image_intensities[:, c_pos, t_pos, int(y_pos), :]
+                    zx_slice = rescale(zx_slice, (voxel_size[0] / voxel_size[2], 1), anti_aliasing=True, preserve_range=True)
+                    zx_slice = zx_slice.astype(image_intensities.dtype)
+                    zx_slice = np.reshape(zx_slice, (1, 1, 1, zx_slice.shape[0], zx_slice.shape[1]))
+                    zx_image = ot.create_image_from_numpy_array(
+                        connection=conn,
+                        data=zx_slice,
+                        image_name=f"{image.getName()}_{shape_comment}_C{c_pos}_zx",
+                        image_description=f"zx Orthogonal slice at Z={z_pos}, C={c_pos}, T={t_pos}. source imageId:{image.getId()}",
+                        # channel_labels=None,
+                        dataset=dataset,
+                        source_image_id=image.getId(),
+                        # channels_list=None,
+                        force_whole_planes=True
+                    )
+                    zx_image.unload()
+                    zx_line_roi = ot.create_roi(
+                        connection=conn,
+                        image=zx_image,
+                        shapes=[
+                            ot.create_shape_line(
+                                x1_pos=0,
+                                y1_pos=(z_pos - 0.5) * (voxel_size[0] / voxel_size[2]),
+                                x2_pos=zx_slice.shape[4],
+                                y2_pos=(z_pos - 0.5) * (voxel_size[0] / voxel_size[2]),
+                                stroke_color=(255, 0, 0, 200),
+                                stroke_width=0.5,
+                            ),
+                            ot.create_shape_line(
+                                x1_pos=x_pos,
+                                y1_pos=0,
+                                x2_pos=x_pos,
+                                y2_pos=zx_slice.shape[3],
+                                stroke_color=(255, 0, 0, 200),
+                                stroke_width=0.5,
+                            )
+                        ]
+                    )
+    
+                    zy_slice = image_intensities[:, c_pos, t_pos, :, int(x_pos)]
+                    zy_slice = rescale(zy_slice, (voxel_size[0] / voxel_size[1], 1), anti_aliasing=True, preserve_range=True)
+                    zy_slice = zy_slice.astype(image_intensities.dtype)
+                    zy_slice = np.transpose(zy_slice, (1, 0))  # Transpose to get the correct orientation
+                    zy_slice = np.reshape(zy_slice, (1, 1, 1, zy_slice.shape[0], zy_slice.shape[1]))
+                    zy_image = ot.create_image_from_numpy_array(
+                        connection=conn,
+                        data=zy_slice,
+                        image_name=f"{image.getName()}_{shape_comment}_C{c_pos}_zy",
+                        image_description=f"zy Orthogonal slice at Z={z_pos}, C={c_pos}, T={t_pos}. source imageId:{image.getId()}",
+                        # channel_labels=None,
+                        dataset=dataset,
+                        source_image_id=image.getId(),
+                        # channels_list=None,
+                        force_whole_planes=True
+                    )
+                    zy_image.unload()
+                    zy_line_roi = ot.create_roi(
+                        connection=conn,
+                        image=zy_image,
+                        shapes=[
+                            ot.create_shape_line(
+                                x1_pos=0,
+                                y1_pos=y_pos,
+                                x2_pos=zy_slice.shape[4],
+                                y2_pos=y_pos,
+                                stroke_color=(255, 0, 0, 200),
+                                stroke_width=0.5,
+                            ),
+                            ot.create_shape_line(
+                                x1_pos=(z_pos - 0.5) * (voxel_size[0] / voxel_size[1]),
+                                y1_pos=0,
+                                x2_pos=(z_pos - 0.5) * (voxel_size[0] / voxel_size[1]),
+                                y2_pos=zy_slice.shape[3],
+                                stroke_color=(255, 0, 0, 200),
+                                stroke_width=0.5,
+                            )
+                        ]
+                    )
+    
+                    profiles = extract_profiles(
+                        image_intensities[:, c_pos, t_pos, :, :],
+                        (z_pos, int(y_pos), int(x_pos)),
+                        21,
+                        voxel_size=voxel_size,)
+                    for axis, profile_img in profiles.items():
+                        # Convert to RGB for OMERO
+                        omero_rgb_profile = np.transpose(profile_img, (2, 0, 1))
+                        omero_rgb_profile = omero_rgb_profile.reshape(
+                            (1, 3, 1, omero_rgb_profile.shape[1], omero_rgb_profile.shape[2])
+                        )
+                        profile_image = ot.create_image_from_numpy_array(
+                            connection=conn,
+                            data=omero_rgb_profile,
+                            image_name=f"{image.getName()}_{shape_comment}_C{c_pos}_{axis}_profile",
+                            image_description=f"Profile along {axis} axis at Z={z_pos}, C={c_pos}, T={t_pos}. source imageId:{image.getId()}",
+                            dataset=dataset,
+                        )
+                        profile_image.unload()
 
 finally:
     conn.close()
